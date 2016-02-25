6d2c9ca749cbd200252a19fe37216bfe1b29abc9
2014arki <2014arki@gmail.com>
2016-02-25 12:22:15 +0300

    "upload_homework_p_distance_corrections_of_mistakes"

--- untitled/p_distance.py
+++ untitled/p_distance.py
@@ -10,13 +10,13 @@ def p_distance(str1, str2):
     if len(str1) != len(str2):
         raise ValueError("Requirement is not applicable")
     mismatches = 0
-    p_distance_value = mismatches % len(str1)
-    for a, b in zip((str1, str2)):
+    current_lenght = len(str1)
+    for a, b in zip(str1, str2):
         if a != b and a != "-" and b != "-":
             mismatches += 1
-        if a == b:
-            next(iter)
         if a == "-" or b == "-":
-            str1.split(a) and str2.split(b)
-    if str1 and str2 and mismatches:
-        return p_distance_value
+            current_lenght -= 1
+    if current_lenght:
+        return mismatches / current_lenght
+    else:
+        raise ValueError("No more string")
