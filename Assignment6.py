3fc7eea8c37861b2a8af290a8ce01e11a06f0bc8
2014arki <2014arki@gmail.com>
2016-03-21 00:00:45 +0300

    "Assignment6_some_new_corrections"

--- Assignment6.py
+++ Assignment6.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from __future__ import division, print_function
+import itertools
 
 # task 1
 
@@ -16,19 +17,23 @@ def groups_for_graph(graph):
     to_visit = []
     visited = []
     for (node, neighbours) in graph.iteritems():
-        if node not in set(list_of_groups):
-            to_visit.append(node)
-            to_visit.append(neighbours)
-            while len(to_visit):
-                group = (((visited.append(node) or visited.append(neighbours[-1]) for node, neighbours in to_visit
-                       if node and neighbours[-1] not in visited)))
-                for reviewed_node in to_visit:
-                    if reviewed_node in visited:
-                        to_visit.remove(reviewed_node)
-                    if not len(neighbours):
-                        to_visit.remove(neighbours)
-                else:
-                    list_of_groups.append(group)
+        for group in list_of_groups:
+            for group_node in group:
+                if group_node != node:
+                    to_visit.append(node)
+                    to_visit.extend(itertools.islice(neighbours, 0, len(neighbours)))
+                    for to_vis_node in to_visit:
+                        if to_vis_node is to_visit[-1]:
+                            set_visited = set(visited)
+                            if to_vis_node in graph and to_vis_node not in set_visited:
+                                next_neighbours = graph[to_vis_node]
+                                visited.append(to_visit.pop())
+                                to_visit.extend(itertools.islice(next_neighbours, 0, len(next_neighbours)))
+                            else:
+                                to_visit.pop()
+                    if not to_visit:
+                        list_of_groups.append(visited)
+                        visited = []
     return list_of_groups
 
 
